b9075922b854cbc92516f81ef028c9aba3f496a8
Turobjon <avezovzamir337@gmail.com>
2024-06-26 16:49:59 +0500

    dfg

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let cards = document.getElementById('cards')
 
-let url = 'https://jsonplaceholder.typicode.com/comments'
+let url = 'https://dummyjson.com/users'
 
 fetch(url)
 .then((res) => res.json())
-.then((data) => getuser((data)) ); 
+.then((param) => getuser(param) ); 
 
-function getuser(param) {
-
-    console.log(param);
+function getuser(data) {
+    //  getuser()
+    console.log(data);
     for (const iterator of param ) {
         let card = document.createElement('div')
-        let name = document.createElement('h4')
-        let email = document.createElement('p')
+        let name = document.createElement('h1')
+        let email = document.createElement('h3')
         let body = document.createElement('p')
 
-        name.innerHTML = iterator?.name
-        email.innerHTML = iterator?.email
-        body.innerHTML = iterator?.body
+        name.innerHTML = iterator?.firstName
+        email.innerHTML = iterator?.lastName    
+        body.innerHTML = iterator?.age
 
         card.classList.add('cards')
         cards.classList.add('cardss')
@@ -25,9 +25,19 @@ function getuser(param) {
         card.appendChild(name)
         card.appendChild(email)
         card.appendChild(body)
-        cards.appendChild(card)
+        cards.appendChild(card) 
         
-
+        filterdata()
 
     }
 }
+function filterdata() {
+    fetch(url)
+.then((res) => res.json())
+.then((res) =>{
+    let filter_users = res.users.filter((item) => item.age < 40)
+} )
+}
+document.getElementById('btn').addEventListener('click',()=>{
+    filterdata()
+})
